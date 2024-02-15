8e368b333df558638ec48c577a01d5b20292eb62
NeyLemes <131485382+NeyLemes@users.noreply.github.com>
2024-02-15 15:38:49 -0300

    Add files via upload

---
+++ desafio python animal.py
@@ -0,0 +1,26 @@
+a = input() 
+b = input() 
+c = input() 
+
+if a == 'vertebrado': 
+  if b == 'ave':
+    if c =='carnivoro':
+      print ( "aguia")
+    elif c == 'onivoro':
+      print ("pomba") 
+  elif b == 'mamifero':
+    if c == 'onivero':
+      print ("homem")
+    elif c =='herbivaro':
+      print ("vaca")
+elif a == 'invertebrado':  
+  if b == 'inseto':
+    if c == ('hematofago'):
+      print ("pulga")
+    elif c == 'herbivaro':
+      print ("lagarta")
+  elif b == 'anelideo':
+    if c == 'hematogafo':
+      print ("sanguessuga")
+    elif c == 'onivoro':
+      print ( "minhoca")     
